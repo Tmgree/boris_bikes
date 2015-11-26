284bcd5f6d48a041aae887e811e1c0adce937d24
tmgree <tmgree@outlook.com>
2015-11-26 15:18:28 +0000

    gg

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,9 +2,13 @@ require_relative 'bike'
 
 
 class DockingStation
-  attr_reader :bikes
-  def initialize
+
+  attr_accessor :capacity
+attr_reader :bikes
+  DEFAULT_CAPACITY=20
+  def initialize(capacity=DEFAULT_CAPACITY)
     @bikes=[]
+    @capacity=capacity
   end
 def release_bike
   fail "no bikes" if empty?
@@ -16,13 +20,14 @@ def dock(bike)
   @bikes << bike
 end
 private
+
 def empty?
 @bikes.size == 0 ? true : false
 
 end
 
 def full?
-  @bikes.size == 20 ? true : false
+  @bikes.size >= @capacity ? true : false
 end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -23,6 +23,30 @@ subject.dock(bike)
 expect(subject.release_bike).to eq bike
 end
 
+it "If nothing is set it will use defult capacity" do
+
+expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
+end
+
+it "The user is allowed to set the capacity" do
+
+station=DockingStation.new(50)
+50.times {station.dock Bike.new}
+expect{ station.dock Bike.new }.to raise_error 'error full'
+
+
+end
+
+describe 'initialization' do
+  subject { DockingStation.new }
+  let(:bike) { Bike.new }
+  it 'defaults capacity' do
+    DockingStation::DEFAULT_CAPACITY.times do
+      subject.dock(bike)
+    end
+    expect{ subject.dock(bike) }.to raise_error 'error full'
+  end
+end
 
 describe '#release_bike' do
   it 'releases a bike' do
@@ -36,15 +60,12 @@ describe '#release_bike' do
     expect{ subject.release_bike}.to raise_error("no bikes")
   end
 it 'error too many bikes docked' do
-  20.times {subject.dock(Bike.new)}
+
+  subject.capacity.times {subject.dock(Bike.new)}
 
   expect{subject.dock(Bike.new)}.to raise_error("error full")
 end
 
 end
-
-
-
-
 
 end
